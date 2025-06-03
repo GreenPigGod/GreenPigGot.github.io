080b187914cdb10874b99733ce18fffe1aad3ccb
GreenPigGod <shenmingtianzhong450@gmail.com>
2025-06-03 17:24:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.onload = function () {
-  const params = new URLSearchParams(window.location.search);
-  const code = params.get("code");
-  const codeElement = document.getElementById("code");
-  if (code) {
-    codeElement.textContent = code;
-  } else {
-    codeElement.textContent = "コードが見つかりませんでした。";
-  }
-};
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("ポートフォリオページが読み込まれました！");
+});
